021d43278012dd78f0ad5b2ef69bd4e7be8be5e9
inglyd <inglydalves@outlook.com>
2021-07-19 16:25:40 -0300

    create: crud de formatos

--- src/routes/formats.routes.js
+++ src/routes/formats.routes.js
@@ -3,6 +3,9 @@ import formats from '../controllers/formats.js';
 
 const routesFormats = express.Router();
 
-routesFormats.get('/formats', formats.findAll);
+routesFormats.get('/formats/:id', formats.show);
+routesFormats.post("/formats", formats.create);
+routesFormats.put("/formats/:id", formats.update);
+routesFormat.delete("/formats/:id", formats.deleteFormat);
 
 export { routesFormats as default };
