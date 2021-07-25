f8ca174cd46265631ca59ebbf47d062980895927
inglyd <inglydalves@outlook.com>
2021-07-25 10:39:18 -0300

    .

--- src/controllers/formats.js
+++ src/controllers/formats.js
@@ -1,7 +1,12 @@
 import Format from '../Models/formatsModel.js';
 
 async function findAll(req, res) {
-  const formats = await Format.findAll();
-  res.json(formats);
+  try {
+    const formats = await Format.findAll();
+    res.json (formats);
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
 }
+
 export default { findAll };
